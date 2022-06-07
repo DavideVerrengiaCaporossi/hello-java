c6c5c5a19029690429ff955b8bc427c78816ef75
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-07 14:35:17 +0200

    primo commit

---
+++ src/org/generation/italy/EsercizioOpertatoriRelazionali.java
@@ -0,0 +1,26 @@
+package org.generation.italy;
+
+public class EsercizioOpertatoriRelazionali {
+	
+ public static void main(String[] args) {
+	
+
+	int x = 3 ;
+	int y = 5 ;
+	
+	if (x == y ) {
+		System.out.println("x uguale a y");
+	}
+	else if (x < y) {
+		System.out.println("x minoredi y ");
+	}
+	else {
+		System.out.println("x maggiore di y ");
+	}
+	
+	 System.out.println("CIAO");
+		
+	}
+}
+
+
